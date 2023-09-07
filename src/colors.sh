77a1b0efe86b35b970eac660e54e4cd1c5f41121
katarn <katarn@katarn.es>
2023-09-07 20:29:12 +0200

    Better output colors & symbols

--- src/colors.sh
+++ src/colors.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+export COLOR_DEFAULT="\e[0m"
+export COLOR_BOLD="\e[1m"
+export COLOR_FAINT="\e[2m"
 export COLOR_FAILED="\e[31m"
 export COLOR_PASSED="\e[32m"
-export COLOR_DEFAULT="\e[0m"
+export COLOR_ALL_PASSED="\e[42m"
