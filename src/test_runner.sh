e5a65da53127b12838a68b5c6366b98325f5d9cd
Chemaclass <chemaclass@outlook.es>
2023-09-07 20:59:26 +0200

    Display the test script only if after filtering there are tests to run

--- src/test_runner.sh
+++ src/test_runner.sh
@@ -6,6 +6,7 @@ callTestFunctions() {
   local script="$1"
   local filter="$2"
   local prefix="test"
+  local ran_functions=false
 
   # Use declare -F to list all function names
   local function_names=$(declare -F | awk '{print $3}')
@@ -16,12 +17,24 @@ callTestFunctions() {
       local filter_lower=$(echo "$filter" | tr '[:upper:]' '[:lower:]')
 
       if [[ -z $filter || $func_name_lower == *"$filter_lower"* ]]; then
-        "$func_name" # Call the function
-        unset "$func_name"
+        functions_to_run+=("$func_name")  # Add eligible function to the array
+        ran_functions=true
       fi
     fi
   done
+
+  if [ "$ran_functions" == true ]; then
+    echo "Running $script"
+    for func_name in "${functions_to_run[@]}"; do
+      "$func_name"  # Call the function
+      unset "$func_name"
+    done
+  fi
 }
+
+###############
+#### MAIN #####
+###############
 
 FILES=()
 FILTER=""
@@ -48,9 +61,8 @@ if [ ${#FILES[@]} -eq 0 ]; then
     exit 1
 fi
 
-# Loop through the test scripts and call test functions
+# Print the "Running $script" message before entering the loop
 for test_script in "${FILES[@]}"; do
-  echo "Running $test_script"
   # shellcheck disable=SC1090
   source "$test_script"
   callTestFunctions "$test_script" "$FILTER"
