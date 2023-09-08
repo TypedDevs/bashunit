bdec6d0266030363fcb0c20062c0a56d63360fb3
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2023-09-08 13:53:43 +0200

    Update src/console_results.sh
    Co-authored-by: Jose Maria Valera Reales <chemaclass@outlook.es>

--- src/console_results.sh
+++ src/console_results.sh
@@ -19,5 +19,5 @@ ${COLOR_FAINT}Total assertions:${COLOR_DEFAULT} ${COLOR_BOLD}${totalAssertions}$
   fi
 }
 
-# Set a trap to call render_result when the script exits
+# Set a trap to call renderResult when the script exits
 trap 'renderResult $_TOTAL_TESTS $_TOTAL_ASSERTIONS_PASSED $_TOTAL_ASSERTIONS_FAILED' EXIT
