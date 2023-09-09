cc1e87ab33eb19c4f8cba7b68466d04b3cd2ce54
Chemaclass <chemaclass@outlook.es>
2023-09-09 11:10:45 +0200

    Fix check_os, export OS

--- src/check_os.sh
+++ src/check_os.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
-function checkOs() {
-  OS="Unknown"
+export OS="Unknown"
 
-  if [ "$(uname)" == "Linux" ]; then
-      OS="Linux"
-  elif [ "$(uname)" == "Darwin" ]; then
-      OS="OSX"
-  elif [[ $(uname) == *"MINGW"* ]]; then
-      OS="Windows"
-  fi
-}
-
-export OS=checkOs
+if [ "$(uname)" == "Linux" ]; then
+    OS="Linux"
+elif [ "$(uname)" == "Darwin" ]; then
+    OS="OSX"
+elif [[ $(uname) == *"MINGW"* ]]; then
+    OS="Windows"
+fi
