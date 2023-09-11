c6907d8ed390877365974c7bc2bdf671d162aee1
Chemaclass <chemaclass@outlook.es>
2023-09-11 13:33:53 +0200

    Move normalizeFunctionName to helpers.sh

--- src/assert.sh
+++ src/assert.sh
@@ -1,21 +1,5 @@
 #!/bin/bash
 
-function normalizeFunctionName() {
-  local original_function_name="$1"
-  local result
-
-  # Remove "test_" prefix
-  result="${original_function_name#test_}"
-  # Replace underscores with spaces
-  result="${result//_/ }"
-  # Remove "test" prefix
-  result="${result#test}"
-  # Capitalize the first letter
-  result="$(tr '[:lower:]' '[:upper:]' <<< "${result:0:1}")${result:1}"
-
-  echo "$result"
-}
-
 function assertEquals() {
   local expected="$1"
   local actual="$2"
