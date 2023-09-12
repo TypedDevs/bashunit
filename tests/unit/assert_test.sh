19276e8af342e83073ff655b4f07b8da47ea5f9b
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2023-09-12 13:09:14 +0200

    chore: simplify assert

--- tests/unit/assert_test.sh
+++ tests/unit/assert_test.sh
@@ -3,55 +3,55 @@ SUCCESSFUL_EMPTY_MESSAGE=""
 
 function test_successful_assertEquals() {
   assertEquals "$SUCCESSFUL_EMPTY_MESSAGE" "$(assertEquals "1" "1")"
-  assertExitCode "0" "$(assertEquals "1" "1")"
+  assertSuccessfulCode "$(assertEquals "1" "1")"
 }
 
 function test_unsuccessful_assertEquals() {
   assertEquals "$(printFailedTest "Unsuccessful assertEquals" "1" "but got" "2")"\
   "$(assertEquals "1" "2")"
 
-  assertExitCode "1" "$(assertEquals "1" "2")"
+  assertGeneralError "$(assertEquals "1" "2")"
 }
 
 function test_successful_assertContains() {
   assertEquals "$SUCCESSFUL_EMPTY_MESSAGE" "$(assertContains "Linux" "GNU/Linux")"
-  assertExitCode "0" "$(assertContains "Linux" "GNU/Linux")"
+  assertSuccessfulCode "$(assertContains "Linux" "GNU/Linux")"
 }
 
 function test_unsuccessful_assertContains() {
   assertEquals "$(printFailedTest "Unsuccessful assertContains" "GNU/Linux" "to contain" "Unix")"\
   "$(assertContains "Unix" "GNU/Linux")"
 
-  assertExitCode "1" "$(assertContains "Unix" "GNU/Linux")"
+  assertGeneralError "$(assertContains "Unix" "GNU/Linux")"
 }
 
 function test_successful_assertNotContains() {
   assertEquals "$SUCCESSFUL_EMPTY_MESSAGE" "$(assertNotContains "Linus" "GNU/Linux")"
-  assertExitCode "0" "$(assertNotContains "Linus" "GNU/Linux")"
+  assertSuccessfulCode "$(assertNotContains "Linus" "GNU/Linux")"
 }
 
 function test_unsuccessful_assertNotContains() {
   assertEquals "$(printFailedTest "Unsuccessful assertNotContains" "GNU/Linux" "to not contain" "Linux")"\
   "$(assertNotContains "Linux" "GNU/Linux")"
 
-  assertExitCode "1" "$(assertNotContains "Linux" "GNU/Linux")"
+  assertGeneralError "$(assertNotContains "Linux" "GNU/Linux")"
 }
 
 function test_successful_assertMatches() {
   assertEquals "$SUCCESSFUL_EMPTY_MESSAGE" "$(assertMatches ".*Linu*" "GNU/Linux")"
-  assertExitCode "0" "$(assertMatches ".*Linu*" "GNU/Linux")"
+  assertSuccessfulCode "$(assertMatches ".*Linu*" "GNU/Linux")"
 }
 
 function test_unsuccessful_assertMatches() {
   assertEquals "$(printFailedTest "Unsuccessful assertMatches" "GNU/Linux" "to match" ".*Pinux*")"\
   "$(assertMatches ".*Pinux*" "GNU/Linux")"
 
-  assertExitCode "1" "$(assertMatches ".*Pinux*" "GNU/Linux")"
+  assertGeneralError "$(assertMatches ".*Pinux*" "GNU/Linux")"
 }
 
 function test_successful_assertNotMatches() {
   assertEquals "$SUCCESSFUL_EMPTY_MESSAGE" "$(assertNotMatches ".*Pinux*" "GNU/Linux")"
-  assertExitCode "0" "$(assertNotMatches ".*Pinux*" "GNU/Linux")"
+  assertSuccessfulCode "$(assertNotMatches ".*Pinux*" "GNU/Linux")"
 }
 
 function test_unsuccessful_assertNotMatches() {
@@ -59,7 +59,7 @@ function test_unsuccessful_assertNotMatches() {
     "$(printFailedTest "Unsuccessful assertNotMatches" "GNU/Linux" "to not match" ".*Linu*")"\
     "$(assertNotMatches ".*Linu*" "GNU/Linux")"
 
-    assertExitCode "1" "$(assertNotMatches ".*Linu*" "GNU/Linux")"
+    assertGeneralError "$(assertNotMatches ".*Linu*" "GNU/Linux")"
 }
 
 function test_successful_assertExitCode() {
@@ -68,7 +68,7 @@ function test_successful_assertExitCode() {
   }
   assertEquals "$SUCCESSFUL_EMPTY_MESSAGE" "$(assertExitCode "0" "$(fake_function)")"
 
-  assertExitCode "0" "$(fake_function)"
+  assertSuccessfulCode "$(fake_function)"
 }
 
 function test_unsuccessful_assertExitCode() {
@@ -79,7 +79,7 @@ function test_unsuccessful_assertExitCode() {
     "$(printFailedTest "Unsuccessful assertExitCode" "1" "to be" "0")"\
     "$(assertExitCode "0" "$(fake_function)")"
 
-  assertExitCode "1" "$(assertExitCode "0" "$(fake_function)")"
+  assertGeneralError "$(assertExitCode "0" "$(fake_function)")"
 }
 
 function test_successful_return_assertExitCode() {
@@ -115,7 +115,7 @@ function test_unsuccessful_assertSuccessfulCode() {
     "$(printFailedTest "Unsuccessful assertSuccessfulCode" "2" "to be exactly" "0")"\
     "$(assertSuccessfulCode "$(fake_function)")"
 
-  assertExitCode "1" "$(assertSuccessfulCode "$(fake_function)")"
+  assertGeneralError "$(assertSuccessfulCode "$(fake_function)")"
 }
 
 function test_successful_assertGeneralError() {
