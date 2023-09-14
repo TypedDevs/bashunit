e46f0dca1cf557afb08b8225157f95330a00b947
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2023-09-14 13:37:14 +0200

    chore: acceptance test for bashunit

---
+++ tests/acceptance/bashunit_test.sh
@@ -0,0 +1,27 @@
+function test_bash_unit_when_a_test_passes() {
+  local test_file=./tests/acceptance/fake_success_test.sh
+  local fixture_route=./tests/acceptance/fixtures/assert_success
+  fixture=$(cat $fixture_route)
+  echo "
+#!/bin/bash
+function test_succeed() { assertEquals \"1\" \"1\" ; }" > $test_file
+
+  assertContains\
+   "$fixture"\
+    "$(./bashunit "$test_file")"
+  rm $test_file
+}
+
+function test_bash_unit_when_a_test_fail() {
+  local test_file=./tests/acceptance/fake_fail_test.sh
+  local fixture_route=./tests/acceptance/fixtures/assert_fail
+  fixture=$(cat $fixture_route)
+  echo "
+#!/bin/bash
+function test_fail() { assertEquals \"1\" \"0\" ; }" > $test_file
+
+  assertContains\
+   "$fixture"\
+    "$(./bashunit "$test_file")"
+  rm $test_file
+}
