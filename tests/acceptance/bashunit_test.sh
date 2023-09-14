e08f3f4d2afdbc25c0a61beca82f45ed1ac8d3c3
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2023-09-14 18:32:02 +0200

    chore: test fix for escape

--- tests/acceptance/bashunit_test.sh
+++ tests/acceptance/bashunit_test.sh
@@ -3,7 +3,8 @@
 function test_bash_unit_when_a_test_passes() {
   local test_file=./tests/acceptance/fake_success_test.sh
   local fixture_route="./tests/acceptance/fixtures/$_OS/assert_success"
-  fixture=$(cat "$fixture_route")
+  # shellcheck disable=SC2059
+  fixture=$(printf "$(cat "$fixture_route")")
   echo "
 #!/bin/bash
 function test_succeed() { assertEquals \"1\" \"1\" ; }" > $test_file
@@ -18,7 +19,7 @@ function test_succeed() { assertEquals \"1\" \"1\" ; }" > $test_file
 function test_bash_unit_when_a_test_fail() {
   local test_file=./tests/acceptance/fake_fail_test.sh
   local fixture_route="./tests/acceptance/fixtures/$_OS/assert_fail"
-  fixture=$(cat "$fixture_route")
+  fixture=$(printf "$(cat "$fixture_route")")
   echo "
 #!/bin/bash
 function test_fail() { assertEquals \"1\" \"0\" ; }" > $test_file
