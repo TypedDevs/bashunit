03af1131878fdcfe2684538817a717f1d6787749
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2023-09-14 18:36:46 +0200

    chore: test fix for escape

--- tests/acceptance/bashunit_test.sh
+++ tests/acceptance/bashunit_test.sh
@@ -2,9 +2,13 @@
 
 function test_bash_unit_when_a_test_passes() {
   local test_file=./tests/acceptance/fake_success_test.sh
-  local fixture_route="./tests/acceptance/fixtures/$_OS/assert_success"
-  # shellcheck disable=SC2059
-  fixture=$(printf "$(cat "$fixture_route")")
+  #local fixture_route="./tests/acceptance/fixtures/$_OS/assert_success"
+  fixture=$(printf "Running ./tests/acceptance/fake_success_test.sh
+\e[32m✓ Passed\e[0m: Succeed
+
+\e[2mTests:     \e[0m \e[32m1 passed\e[0m, 1 total
+\e[2mAssertions:\e[0m \e[32m1 passed\e[0m, 1 total
+\e[42mAll tests passed\e[0m")
   echo "
 #!/bin/bash
 function test_succeed() { assertEquals \"1\" \"1\" ; }" > $test_file
@@ -18,8 +22,14 @@ function test_succeed() { assertEquals \"1\" \"1\" ; }" > $test_file
 
 function test_bash_unit_when_a_test_fail() {
   local test_file=./tests/acceptance/fake_fail_test.sh
-  local fixture_route="./tests/acceptance/fixtures/$_OS/assert_fail"
-  fixture=$(printf "$(cat "$fixture_route")")
+  #local fixture_route="./tests/acceptance/fixtures/$_OS/assert_fail"
+  fixture=$(printf "Running ./tests/acceptance/fake_fail_test.sh
+\e[31m✗ Failed\e[0m: Fail
+    \e[2mExpected\e[0m \e[1m'1'\e[0m
+    \e[2mbut got\e[0m \e[1m'0'\e[0m
+
+\e[2mTests:     \e[0m \e[31m1 failed\e[0m, 1 total
+\e[2mAssertions:\e[0m \e[31m1 failed\e[0m, 1 total")
   echo "
 #!/bin/bash
 function test_fail() { assertEquals \"1\" \"0\" ; }" > $test_file
