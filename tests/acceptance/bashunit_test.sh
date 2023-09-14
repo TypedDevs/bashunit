3deec0751198aaaf5a3cfcd5a0d025a6f8c9d526
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2023-09-14 18:17:52 +0200

    chore: add different fixtures per OS to see how Windows handles the output

--- tests/acceptance/bashunit_test.sh
+++ tests/acceptance/bashunit_test.sh
@@ -2,8 +2,8 @@
 
 function test_bash_unit_when_a_test_passes() {
   local test_file=./tests/acceptance/fake_success_test.sh
-  local fixture_route=./tests/acceptance/fixtures/assert_success
-  fixture=$(cat $fixture_route)
+  local fixture_route="./tests/acceptance/fixtures/$_OS/assert_success"
+  fixture=$(cat "$fixture_route")
   echo "
 #!/bin/bash
 function test_succeed() { assertEquals \"1\" \"1\" ; }" > $test_file
@@ -11,13 +11,14 @@ function test_succeed() { assertEquals \"1\" \"1\" ; }" > $test_file
   assertContains\
    "$fixture"\
     "$(./bashunit "$test_file")"
+
   rm $test_file
 }
 
 function test_bash_unit_when_a_test_fail() {
   local test_file=./tests/acceptance/fake_fail_test.sh
-  local fixture_route=./tests/acceptance/fixtures/assert_fail
-  fixture=$(cat $fixture_route)
+  local fixture_route="./tests/acceptance/fixtures/$_OS/assert_fail"
+  fixture=$(cat "$fixture_route")
   echo "
 #!/bin/bash
 function test_fail() { assertEquals \"1\" \"0\" ; }" > $test_file
@@ -25,5 +26,6 @@ function test_fail() { assertEquals \"1\" \"0\" ; }" > $test_file
   assertContains\
    "$fixture"\
     "$(./bashunit "$test_file")"
+
   rm $test_file
 }
