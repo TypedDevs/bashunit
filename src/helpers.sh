5843b427c4b9b282344758e39ae694de2f3a78d1
Chemaclass <chemaclass@outlook.es>
2023-09-16 09:38:56 +0200

    Add docs to helpers functions

--- src/helpers.sh
+++ src/helpers.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #
-# @param string Eg: "test_some_logic"
+# @param $1 string Eg: "test_some_logic_camelCase"
 #
-# @result string Eg: "Some logic"
+# @result string Eg: "Some logic camelCase"
 #
 function Helper::normalizeTestFunctionName() {
   local original_function_name="$1"
@@ -21,6 +21,13 @@ function Helper::normalizeTestFunctionName() {
   echo "$result"
 }
 
+#
+# @param $1 string Eg: "prefix"
+# @param $2 string Eg: "filter"
+# @param $3 array Eg: "[fn1, fn2, prefix_filter_fn3, fn4, ...]"
+#
+# @result array Eg: "[prefix_filter_fn3, ...]" The filtered functions with prefix
+#
 function Helper::getFunctionsToRun() {
   local prefix=$1
   local filter=$2
@@ -52,6 +59,9 @@ function Helper::getFunctionsToRun() {
   echo "${functions_to_run[@]}"
 }
 
+#
+# @param $1 string Eg: "do_something"
+#
 function Helper::executeFunctionIfExists() {
   local function_name=$1
 
@@ -60,6 +70,9 @@ function Helper::executeFunctionIfExists() {
   fi
 }
 
+#
+# @param $1 string Eg: "do_something"
+#
 function Helper::unsetIfExists() {
   local function_name=$1
 
