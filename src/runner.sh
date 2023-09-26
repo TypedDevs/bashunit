48b7b73cab30ce2d00e48494bc0a6e2a5f6ab29f
Chemaclass <chemaclass@outlook.es>
2023-09-26 18:14:50 +0200

    Extract main from runner.sh to bashunit entrypoint

--- src/runner.sh
+++ src/runner.sh
@@ -2,6 +2,34 @@
 
 # shellcheck disable=SC2317
 
+function Runner::loadTestFiles() {
+  local filter=$1
+  local files=("${@:2}") # Store all arguments starting from the second as an array
+
+  if [[ ${#files[@]} == 0 ]]; then
+    printf "%sError: At least one file path is required.%s\n" "${_COLOR_FAILED}" "${_COLOR_DEFAULT}"
+    printf "%sUsage: %s <test_file.sh>%s\n" "${_COLOR_DEFAULT}" "$0" "${_COLOR_DEFAULT}"
+    exit 1
+  fi
+
+  for test_file in "${files[@]}"; do
+    if [[ ! -f $test_file ]]; then
+      continue
+    fi
+    # shellcheck disable=SC1090
+    #shellcheck source=/dev/null
+    source "$test_file"
+
+    Runner::runSetUpBeforeScript
+    Runner::callTestFunctions "$test_file" "$filter"
+    if [ "$PARALLEL_RUN" = true ] ; then
+      wait
+    fi
+    Runner::runTearDownAfterScript
+    Runner::cleanSetUpAndTearDownAfterScript
+  done
+}
+
 function Runner::callTestFunctions() {
   local script="$1"
   local filter="$2"
@@ -43,33 +71,6 @@ function Runner::runTest() {
   Console::printSuccessfulTest "${label}"
   State::addTestsPassed
 }
-function Runner::loadTestFiles() {
-  local filter=$1
-  local files=("${@:2}") # Store all arguments starting from the second as an array
-
-  if [[ ${#files[@]} == 0 ]]; then
-    printf "%sError: At least one file path is required.%s\n" "${_COLOR_FAILED}" "${_COLOR_DEFAULT}"
-    printf "%sUsage: %s <test_file.sh>%s\n" "${_COLOR_DEFAULT}" "$0" "${_COLOR_DEFAULT}"
-    exit 1
-  fi
-
-  for test_file in "${files[@]}"; do
-    if [[ ! -f $test_file ]]; then
-      continue
-    fi
-    # shellcheck disable=SC1090
-    #shellcheck source=/dev/null
-    source "$test_file"
-
-    Runner::runSetUpBeforeScript
-    Runner::callTestFunctions "$test_file" "$filter"
-    if [ "$PARALLEL_RUN" = true ] ; then
-      wait
-    fi
-    Runner::runTearDownAfterScript
-    Runner::cleanSetUpAndTearDownAfterScript
-  done
-}
 
 function Runner::runSetUp() {
   Helper::executeFunctionIfExists 'setUp'
@@ -96,28 +97,3 @@ function Runner::cleanSetUpAndTearDownAfterScript() {
   Helper::unsetIfExists 'setUpBeforeScript'
   Helper::unsetIfExists 'tearDownAfterScript'
 }
-
-###############
-#### MAIN #####
-###############
-
-_FILTER=""
-_FILES=()
-
-while [[ $# -gt 0 ]]; do
-  argument="$1"
-  case $argument in
-    --filter)
-      _FILTER="$2"
-      shift
-      shift
-      ;;
-    *)
-      _FILES+=("$argument")
-      shift
-      ;;
-  esac
-done
-
-Runner::loadTestFiles "$_FILTER" "${_FILES[@]}"
-
