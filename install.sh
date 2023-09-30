f697f7c556f9cf5cb15a4f5b62a6a6942353051b
Chemaclass <chemaclass@outlook.es>
2023-09-30 16:27:29 +0200

    Split install and build scripts

--- install.sh
+++ install.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 
-mkdir -p bin
+cd "$(dirname "$0")"
+rm -rf lib/bashunit
+mkdir lib
+cd lib
 
-cat src/*.sh > bin/temp.sh
-cat bashunit >> bin/temp.sh
-grep -v '^source' bin/temp.sh > bin/bashunit
-rm bin/temp.sh
+TAG=${1-main}
 
-chmod +x bin/bashunit
+if [[ $TAG == main ]]; then
+  echo "> Using main branch"
+  git clone git@github.com:TypedDevs/bashunit.git temp_bashunit
+else
+  echo "> Using a concrete tag '$TAG'"
+  curl -L -O -J "https://github.com/TypedDevs/bashunit/archive/refs/tags/$TAG.tar.gz"
+  tar -zxvf "bashunit-$TAG.tar.gz"
+  cp "bashunit-$TAG/bin/bashunit" temp_bashunit
+  rm "bashunit-$TAG.tar.gz"
+fi
 
-echo "Build complete. bashunit has been generated in the bin folder."
+## Common
+cd temp_bashunit
+./build.sh
+cd ..
+cp temp_bashunit/bin/bashunit bashunit
+rm -rf temp_bashunit
