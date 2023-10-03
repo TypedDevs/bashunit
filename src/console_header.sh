2077602a146edf8addbf43fd688a1b519b4d563d
Chemaclass <chemaclass@outlook.es>
2023-10-03 11:53:04 +0200

    Add HEADER_ASCII_ART env var

--- src/console_header.sh
+++ src/console_header.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 function console_header::print_version() {
-  cat <<EOF
+  if [[ $HEADER_ASCII_ART == true ]]; then
+    cat <<EOF
  _               _                   _
 | |__   __ _ ___| |__  __ __ ____ (_) |_
 | '_ \ / _' / __| '_ \| | | | '_ \| | __|
 | |_) | (_| \__ \ | | | |_| | | | | | |_
 |_.__/ \__,_|___/_| |_|\___/|_| |_|_|\__|
 EOF
-  printf "%s\n\n" "$BASH_UNIT_VERSION"
+    printf "%s\n\n" "$BASH_UNIT_VERSION"
+  else
+    printf "bashunit - %s\n" "$BASH_UNIT_VERSION"
+  fi
 }
