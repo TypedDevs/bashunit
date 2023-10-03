12839c942b898f738ec19cdec01ce2f8346d16aa
katarn <katarn@katarn.es>
2023-10-03 16:55:41 +0200

    Change color from duplicate test error message

--- src/console_results.sh
+++ src/console_results.sh
@@ -5,7 +5,7 @@ _SUCCESSFUL_TEST_COUNT=0
 
 function console_results::render_result() {
   if [[ "$(state::is_duplicated_test_functions_found)" == true ]]; then
-    printf "%s> Duplicate test functions found%s\n" "${_COLOR_FAILED}" "${_COLOR_DEFAULT}"
+    printf "%sDuplicate test functions found%s\n" "${_COLOR_DUPLICATED}" "${_COLOR_DEFAULT}"
     return
   fi
 
