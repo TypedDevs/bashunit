85269ef679a1d3dbab4945c8bca481e3d7bd3ccf
katarn <katarn@katarn.es>
2023-10-12 14:07:02 +0200

    feat: add blog index

---
+++ docs/posts/PostCard.vue
@@ -0,0 +1,31 @@
+<template>
+  <a :href="post.url">
+    {{ post }}
+    <br>
+  </a>
+</template>
+
+<script lang="ts">
+import {defineComponent, PropType} from "vue";
+
+interface Post {
+  url: string,
+  frontmatter: {
+    title: string,
+    date: string
+  }
+}
+
+export default defineComponent({
+  props: {
+    post: {
+      required: true,
+      type: Object as PropType<Post>
+    }
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
---
+++ docs/posts/index.md
@@ -0,0 +1,14 @@
+# Posts
+
+<PostCard
+  v-for="post of posts"
+  :post="post"
+/>
+
+<script setup>
+import { data } from './posts.data.ts'
+import PostCard from './PostCard.vue'
+
+const posts = data.filter((post) => post.url != '/posts/')
+  .sort((postA, postB) => postA.url < postB.url)
+</script>
