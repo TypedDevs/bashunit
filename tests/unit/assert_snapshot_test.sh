693ec4f44f8ed06c3071db0a393408c2cba52708
Chemaclass <chemaclass@outlook.es>
2023-10-18 07:37:37 +0200

    chore: simplify test_unsuccessful_assert_match_snapshot removing colors

--- tests/unit/assert_snapshot_test.sh
+++ tests/unit/assert_snapshot_test.sh
@@ -6,17 +6,14 @@ function test_successful_assert_match_snapshot() {
 
 function test_unsuccessful_assert_match_snapshot() {
   local expected
-  expected="$(printf "${_COLOR_FAILED}✗ Failed${_COLOR_DEFAULT}: Unsuccessful assert match snapshot
-    ${_COLOR_FAINT}Expected to match the snapshot${_COLOR_DEFAULT}
-    ${_COLOR_FAILED}[-Actual-]${_COLOR_PASSED}{+Expected+}${_COLOR_DEFAULT} snapshot${_COLOR_FAILED}[-text-]${_COLOR_DEFAULT}")"
+  expected="$(printf "✗ Failed: Unsuccessful assert match snapshot
+    Expected to match the snapshot
+    [-Actual-]{+Expected+} snapshot[-text-]")"
 
   local actual
   actual="$(assert_match_snapshot "Expected snapshot")"
-
-  # Remove color codes using sed
-  clean_expected=$(echo -e "$expected" | sed "s/\x1B\[[0-9;]*[JKmsu]//g")
-  clean_actual=$(echo -e "$actual" | sed "s/\x1B\[[0-9;]*[JKmsu]//g")
+  actual_without_colors=$(echo -e "$actual" | sed "s/\x1B\[[0-9;]*[JKmsu]//g")
 
 
-  assert_equals "$clean_expected" "$clean_actual"
+  assert_equals "$expected" "$actual_without_colors"
 }
