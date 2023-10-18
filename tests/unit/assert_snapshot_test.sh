2be8f59c70704bdd1a7f080ddaf308d9f933a3d3
Chemaclass <chemaclass@outlook.es>
2023-10-18 08:27:31 +0200

    chore: assert file exists test_successful_assert_match_snapshot

--- tests/unit/assert_snapshot_test.sh
+++ tests/unit/assert_snapshot_test.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
 function test_successful_assert_match_snapshot() {
+  local snapshot_file_path=tests/unit/snapshots/assert_snapshot_test_sh.test_successful_assert_match_snapshot.snapshot
+
+  [ -f "$snapshot_file_path" ] && rm $snapshot_file_path
+  assert_file_not_exists $snapshot_file_path
+
   assert_empty "$(assert_match_snapshot "Hello World!")"
+  assert_file_exists $snapshot_file_path
+  rm $snapshot_file_path
 }
 
 function test_unsuccessful_assert_match_snapshot() {
@@ -13,7 +20,6 @@ function test_unsuccessful_assert_match_snapshot() {
   local actual
   actual="$(assert_match_snapshot "Expected snapshot")"
   actual_without_colors=$(echo -e "$actual" | sed "s/\x1B\[[0-9;]*[JKmsu]//g")
-
 
   assert_equals "$expected" "$actual_without_colors"
 }
