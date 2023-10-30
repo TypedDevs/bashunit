1b67c6e7d89ad16717cf6194f7c4884d2df66792
Chemaclass <chemaclass@outlook.es>
2023-10-30 14:22:35 +0100

    refactor: install.sh split main to concrete_version

--- install.sh
+++ install.sh
@@ -9,19 +9,32 @@ DIR=${1-lib}
 VERSION=${2-latest}
 TAG="$LATEST_BASHUNIT_VERSION"
 
+function install_main() {
+  echo "> Downloading non-stable main"
+  echo "> bashunit has been installed in the 'lib' folder"
+}
+
+function install_concrete_version() {
+  if [[ $VERSION != 'latest' ]]; then
+    TAG="$VERSION"
+    echo "> Downloading a concrete version: '$TAG'"
+  else
+    echo "> Downloading the latest version: '$TAG'"
+  fi
+
+  curl -L -O -J "https://github.com/TypedDevs/bashunit/releases/download/$TAG/bashunit" 2>/dev/null
+  chmod u+x "bashunit"
+  echo "> bashunit has been installed in the '$DIR' folder"
+}
 
 cd "$(dirname "$0")"
 rm -f "$DIR"/bashunit
 [ -d "$DIR" ] || mkdir "$DIR"
 cd "$DIR"
 
-if [[ $VERSION != 'latest' ]]; then
-  TAG="$VERSION"
-  echo "> Downloading a concrete version: '$TAG'"
+if [[ $VERSION == 'main' ]]; then
+  install_main
 else
-  echo "> Downloading the latest version: '$TAG'"
+  install_concrete_version
 fi
 
-curl -L -O -J "https://github.com/TypedDevs/bashunit/releases/download/$TAG/bashunit" 2>/dev/null
-chmod u+x "bashunit"
-echo "> bashunit has been installed in the '$DIR' folder"
