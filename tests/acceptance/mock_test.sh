cebb41c7afdc5f03b123ddfd99f10f406c11af70
Chemaclass <chemaclass@outlook.es>
2023-10-31 11:03:08 +0100

    chore: use clear_mocks()

--- tests/acceptance/mock_test.sh
+++ tests/acceptance/mock_test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #
-# Make sure that the runner::clean_mocks() is being called and removing the mocks and spies
+# Make sure that the runner::clear_mocks() is being called and removing the mocks and spies
 #
-function test_runner_clean_mocks_1() {
+function test_runner_clear_mocks_1() {
   mock ls echo foo
   assert_is_mock ls
 
@@ -11,7 +11,7 @@ function test_runner_clean_mocks_1() {
   assert_is_mock ps
 }
 
-function test_runner_clean_mocks_2() {
+function test_runner_clear_mocks_2() {
   assert_is_not_mock ls
   assert_is_not_mock ps
 }
