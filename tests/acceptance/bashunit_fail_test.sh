010488efc55a386196902437ef78ab633e8c5045
katarn <katarn@katarn.es>
2023-11-07 12:32:36 +0100

    test: split fail acceptance tests

---
+++ tests/acceptance/bashunit_fail_test.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+function set_up_before_script() {
+  TEST_ENV_FILE="tests/acceptance/fixtures/.env.default"
+  TEST_ENV_FILE_SIMPLE="tests/acceptance/fixtures/.env.simple"
+}
+
+function test_bashunit_when_a_test_fail_verbose_output_env() {
+  local test_file=./tests/acceptance/fixtures/test_bashunit_when_a_test_fail.sh
+
+  assert_match_snapshot "$(./bashunit --env "$TEST_ENV_FILE" "$test_file")"
+  assert_general_error "$(./bashunit --env "$TEST_ENV_FILE" "$test_file")"
+}
+
+function test_bashunit_when_a_test_fail_verbose_output_option() {
+  local test_file=./tests/acceptance/fixtures/test_bashunit_when_a_test_fail.sh
+
+  assert_match_snapshot "$(./bashunit --env "$TEST_ENV_FILE_SIMPLE" "$test_file" --verbose)"
+  assert_general_error "$(./bashunit --env "$TEST_ENV_FILE_SIMPLE" "$test_file" --verbose)"
+}
+
+function test_bashunit_when_a_test_fail_simple_output_env() {
+  todo "Should print something like ...F."
+  return
+
+  local test_file=./tests/acceptance/fixtures/test_bashunit_when_a_test_fail.sh
+
+  assert_match_snapshot "$(./bashunit --env "$TEST_ENV_FILE_SIMPLE" "$test_file")"
+  assert_general_error "$(./bashunit --env "$TEST_ENV_FILE_SIMPLE" "$test_file")"
+}
+
+function test_bashunit_when_a_test_fail_simple_output_option() {
+  todo "Should print something like ...F."
+  return
+
+  local test_file=./tests/acceptance/fixtures/test_bashunit_when_a_test_fail.sh
+
+  assert_match_snapshot "$(./bashunit --env "$TEST_ENV_FILE" "$test_file" --simple)"
+  assert_general_error "$(./bashunit --env "$TEST_ENV_FILE" "$test_file" --simple)"
+}
