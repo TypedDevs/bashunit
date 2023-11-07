0ff512814e0b4709e7e62443bdcfb23951a7fc20
katarn <katarn@katarn.es>
2023-11-07 12:29:05 +0100

    test: split path acceptance tests

--- tests/acceptance/bashunit_test.sh
+++ tests/acceptance/bashunit_test.sh
@@ -3,25 +3,6 @@
 function set_up_before_script() {
   TEST_ENV_FILE="tests/acceptance/fixtures/.env.default"
   TEST_ENV_FILE_SIMPLE="tests/acceptance/fixtures/.env.simple"
-  TEST_ENV_FILE_WITH_PATH="tests/acceptance/fixtures/.env.with_path"
-}
-
-function test_bashunit_without_path_env_nor_argument() {
-  assert_match_snapshot "$(./bashunit --env "$TEST_ENV_FILE")"
-  assert_general_error "$(./bashunit --env "$TEST_ENV_FILE")"
-}
-
-function test_bashunit_with_argument_path() {
-  todo "Here it is supposed to search for files ending in test, this functionality has recently stopped working"
-  return
-
-  assert_match_snapshot "$(./bashunit tests/acceptance/fixtures/tests_path --env "$TEST_ENV_FILE")"
-  assert_general_error "$(./bashunit tests/acceptance/fixtures/tests_path --env "$TEST_ENV_FILE")"
-}
-
-function test_bashunit_with_env_default_path() {
-  assert_match_snapshot "$(./bashunit --env "$TEST_ENV_FILE_WITH_PATH")"
-  assert_successful_code "$(./bashunit --env "$TEST_ENV_FILE_WITH_PATH")"
 }
 
 function test_bashunit_when_a_test_passes_verbose_output_env() {
