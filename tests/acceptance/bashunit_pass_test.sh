36228ad0c3bf50d44786c6c12fda6ac32221be3e
katarn <katarn@katarn.es>
2023-11-07 12:30:34 +0100

    test: split pass acceptance tests

---
+++ tests/acceptance/bashunit_pass_test.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+function set_up_before_script() {
+  TEST_ENV_FILE="tests/acceptance/fixtures/.env.default"
+  TEST_ENV_FILE_SIMPLE="tests/acceptance/fixtures/.env.simple"
+}
+
+function test_bashunit_when_a_test_passes_verbose_output_env() {
+  local test_file=./tests/acceptance/fixtures/test_bashunit_when_a_test_passes.sh
+
+  assert_match_snapshot "$(./bashunit --env "$TEST_ENV_FILE" "$test_file")"
+  assert_successful_code "$(./bashunit --env "$TEST_ENV_FILE" "$test_file")"
+}
+
+function test_bashunit_when_a_test_passes_verbose_output_option() {
+  local test_file=./tests/acceptance/fixtures/test_bashunit_when_a_test_passes.sh
+
+  assert_match_snapshot "$(./bashunit --env "$TEST_ENV_FILE_SIMPLE" "$test_file" --verbose)"
+  assert_successful_code "$(./bashunit --env "$TEST_ENV_FILE_SIMPLE" "$test_file" --verbose)"
+}
+
+function test_bashunit_when_a_test_passes_simple_output_env() {
+  local test_file=./tests/acceptance/fixtures/test_bashunit_when_a_test_passes.sh
+
+  assert_match_snapshot "$(./bashunit --env "$TEST_ENV_FILE_SIMPLE" "$test_file")"
+  assert_successful_code "$(./bashunit --env "$TEST_ENV_FILE_SIMPLE" "$test_file")"
+}
+
+function test_bashunit_when_a_test_passes_simple_output_option() {
+  local test_file=./tests/acceptance/fixtures/test_bashunit_when_a_test_passes.sh
+
+  assert_match_snapshot "$(./bashunit --env "$TEST_ENV_FILE" "$test_file" --simple)"
+  assert_successful_code "$(./bashunit --env "$TEST_ENV_FILE" "$test_file" --simple)"
+}
