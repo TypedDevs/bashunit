e8096caf78c1f9a967960ecea47ef76d2a3b315b
Chemaclass <chemaclass@outlook.es>
2023-11-11 21:47:40 +0100

    chore: prepare todo test_all_tests_files_with_wildcard

--- tests/acceptance/bashunit_find_tests_inside_folder_test.sh
+++ tests/acceptance/bashunit_find_tests_inside_folder_test.sh
@@ -5,13 +5,22 @@ function set_up_before_script() {
 }
 
 function test_all_tests_files_within_a_directory() {
-  local test_dir=./tests/acceptance/fixtures
+  local path="./tests/acceptance/fixtures"
 
-  assert_match_snapshot "$(./bashunit --env "$TEST_ENV_FILE" "$test_dir")"
+  assert_match_snapshot "$(./bashunit --env "$TEST_ENV_FILE" "$path")"
 }
 
 function test_all_tests_files_within_a_file() {
-  local test_dir=./tests/acceptance/fixtures/tests_path/a_test.sh
+  local path="./tests/acceptance/fixtures/tests_path/a_test.sh"
 
-  assert_match_snapshot "$(./bashunit --env "$TEST_ENV_FILE" "$test_dir")"
+  assert_match_snapshot "$(./bashunit --env "$TEST_ENV_FILE" "$path")"
+}
+
+function test_all_tests_files_with_wildcard() {
+  todo "it is not working yet, but I don't know why..."
+  return
+
+  local path="./tests/acceptance/fixtures/tests_path/*_test.sh"
+
+  assert_match_snapshot "$(./bashunit --env "$TEST_ENV_FILE" "$path")"
 }
