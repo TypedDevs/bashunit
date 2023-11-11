d6a6ceaf95c6ec1ba734ec67c40dff89f0855c84
Chemaclass <chemaclass@outlook.es>
2023-11-11 21:39:02 +0100

    test: all_tests_files_within_a_file

--- tests/acceptance/bashunit_find_tests_inside_folder_test.sh
+++ tests/acceptance/bashunit_find_tests_inside_folder_test.sh
@@ -9,3 +9,9 @@ function test_all_tests_files_within_a_directory() {
 
   assert_match_snapshot "$(./bashunit --env "$TEST_ENV_FILE" "$test_dir")"
 }
+
+function test_all_tests_files_within_a_file() {
+  local test_dir=./tests/acceptance/fixtures/tests_path/a_test.sh
+
+  assert_match_snapshot "$(./bashunit --env "$TEST_ENV_FILE" "$test_dir")"
+}
