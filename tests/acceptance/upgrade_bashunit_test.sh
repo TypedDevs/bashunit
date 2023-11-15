34c8a960b66e079b383f86695701d2b3a3c0d1c8
Geethakrishna Puligundla <gpuligundla@clemson.edu>
2023-11-15 06:56:18 -0500

    feat: modified unit test with mock behaviour & fixed sed command

--- tests/acceptance/upgrade_bashunit_test.sh
+++ tests/acceptance/upgrade_bashunit_test.sh
@@ -19,7 +19,7 @@ function test_bashunit_upgrade_on_latest() {
 }
 
 function test_fake_bashunit_upgrade() {
-    sed -i 's/declare -r BASHUNIT_VERSION="[0-9]\{1,\}\.[0-9]\{1,\}\.[0-9]\{1,\}"/declare -r \
+    sed -i -e 's/declare -r BASHUNIT_VERSION="[0-9]\{1,\}\.[0-9]\{1,\}\.[0-9]\{1,\}"/declare -r \
     BASHUNIT_VERSION="0.1.0"/' ./bin/bashunit
 
     output="$(./bin/bashunit --upgrade)"
