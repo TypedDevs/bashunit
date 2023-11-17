fbaa2b8a1a54c9e7dabc49fbaaaa1dee44c54e9f
Chemaclass <chemaclass@outlook.es>
2023-11-17 23:27:30 +0100

    silent test_do_not_update_on_consecutive_calls warnings

--- tests/acceptance/bashunit_upgrade_test.sh
+++ tests/acceptance/bashunit_upgrade_test.sh
@@ -36,23 +36,21 @@ function test_upgrade_when_a_new_version_found() {
 }
 
 function test_do_not_update_on_consecutive_calls() {
-  todo "remove me when --upgrade is released"
-  return
-
-  sed -i -e \
-    's/declare -r BASHUNIT_VERSION="[0-9]\{1,\}\.[0-9]\{1,\}\.[0-9]\{1,\}"/declare -r BASHUNIT_VERSION="0.1.0"/' \
-    ./bin/bashunit
-
-  if [[ $_OS == "OSX" ]]; then
-    rm ./bin/bashunit-e
-  fi
-
-  ./bin/bashunit --upgrade
-  ./bin/bashunit --version
-
-  local output
-  output="$(./bin/bashunit --upgrade)"
-
-  assert_equals "> You are already on latest release" "$output"
-  assert_string_ends_with "$LATEST_VERSION" "$(./bin/bashunit --version --env "$TEST_ENV_FILE")"
+  todo "enable this test when --upgrade is released"
+#  sed -i -e \
+#    's/declare -r BASHUNIT_VERSION="[0-9]\{1,\}\.[0-9]\{1,\}\.[0-9]\{1,\}"/declare -r BASHUNIT_VERSION="0.1.0"/' \
+#    ./bin/bashunit
+#
+#  if [[ $_OS == "OSX" ]]; then
+#    rm ./bin/bashunit-e
+#  fi
+#
+#  ./bin/bashunit --upgrade
+#  ./bin/bashunit --version
+#
+#  local output
+#  output="$(./bin/bashunit --upgrade)"
+#
+#  assert_equals "> You are already on latest release" "$output"
+#  assert_string_ends_with "$LATEST_VERSION" "$(./bin/bashunit --version --env "$TEST_ENV_FILE")"
 }
