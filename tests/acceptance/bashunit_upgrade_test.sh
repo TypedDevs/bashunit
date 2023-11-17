0a7f26b8a01456c6a5ac1682c709b478a8042998
Chemaclass <chemaclass@outlook.es>
2023-11-17 22:29:26 +0100

    refactor: bashunit_upgrade_test naming

--- tests/acceptance/bashunit_upgrade_test.sh
+++ tests/acceptance/bashunit_upgrade_test.sh
@@ -8,14 +8,14 @@ function tear_down() {
   rm -f ./bin/bashunit
 }
 
-function test_bashunit_upgrade_on_latest() {
+function test_do_not_upgrade_when_latest() {
   local output
   output="$(./bin/bashunit --upgrade)"
 
   assert_equals "> You are already on latest release" "$output"
 }
 
-function test_fake_bashunit_upgrade() {
+function test_upgrade_when_a_new_version_found() {
   sed -i -e \
     's/declare -r BASHUNIT_VERSION="[0-9]\{1,\}\.[0-9]\{1,\}\.[0-9]\{1,\}"/declare -r BASHUNIT_VERSION="0.1.0"/' \
     ./bin/bashunit
@@ -29,4 +29,24 @@ function test_fake_bashunit_upgrade() {
 
   assert_contains "> Upgrading bashunit to latest release" "$output"
   assert_contains "> bashunit upgraded successfully to latest version" "$output"
+}
+
+function test_do_not_update_on_consecutive_calls() {
+  skip "until we have --upgrade released"
+  return
+
+  sed -i -e \
+    's/declare -r BASHUNIT_VERSION="[0-9]\{1,\}\.[0-9]\{1,\}\.[0-9]\{1,\}"/declare -r BASHUNIT_VERSION="0.1.0"/' \
+    ./bin/bashunit
+
+  if [[ $_OS == "OSX" ]]; then
+    rm ./bin/bashunit-e
+  fi
+
+  ./bin/bashunit --upgrade
+
+  local output
+  output="$(./bin/bashunit --upgrade)"
+
+  assert_equals "> You are already on latest release" "$output"
 }
