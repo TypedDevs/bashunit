6884e4c12b227e5d40284c895a041f9f9b1311d9
Chemaclass <chemaclass@outlook.es>
2023-12-24 21:41:17 +0100

    fix: helper::get_functions_to_run()

--- src/helpers.sh
+++ src/helpers.sh
@@ -55,30 +55,18 @@ function helper::get_functions_to_run() {
   local filter=$2
   local function_names=$3
 
-  local functions_to_run=()
+  local filtered_functions=""
 
-  for function_name in $function_names; do
-    if [[ $function_name != ${prefix}* ]]; then
-      continue
+  for fn in $function_names; do
+    if [[ $fn == ${prefix}_${filter}* ]]; then
+      if [[ $filtered_functions == *" $fn"* ]]; then
+        return 1
+      fi
+      filtered_functions+=" $fn"
     fi
-
-    local lower_case_function_name
-    lower_case_function_name=$(echo "$function_name" | tr '[:upper:]' '[:lower:]')
-    local lower_case_filter
-    lower_case_filter=$(echo "$filter" | tr '[:upper:]' '[:lower:]')
-
-    if [[ -n $filter && $lower_case_function_name != *"$lower_case_filter"* ]]; then
-      continue
-    fi
-
-    if [[ "${functions_to_run[*]}" =~ ${function_name} ]]; then
-      return 1
-    fi
-
-    functions_to_run+=("$function_name")
   done
 
-  echo "${functions_to_run[@]}"
+  echo "${filtered_functions# }"
 }
 
 #
