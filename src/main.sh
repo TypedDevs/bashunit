ec65d2996ea18f51df3a55464a8350504b2174f1
Chemaclass <chemaclass@outlook.es>
2024-06-16 13:47:48 +0200

    refactor: main.sh

--- src/main.sh
+++ src/main.sh
@@ -2,19 +2,20 @@
 
 function main::exec_tests() {
   local filter=$1
-  local files=("${@:2}") # Store all arguments starting from the second as an array
+  local args=("${@:2}")
 
   console_header::print_version_with_env
-  runner::load_test_files "$filter" "${files[@]}"
+  runner::load_test_files "$filter" "${args[@]}"
   console_results::render_result
   exit 0
 }
 
 function main::exec_assert() {
   local assert_fn=$1
-  local args=("${@:2}") # Store all arguments starting from the second as an array
+  local args=("${@:2}")
 
   "$assert_fn" "${args[@]}"
+
   if [[ "$(state::get_tests_failed)" -gt 0 ]] || [[ "$(state::get_assertions_failed)" -gt 0 ]]; then
       exit 1
   fi
