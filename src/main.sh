cd47b0fb447bd4bbe58a4f3317aa79faf4f18f0a
Chemaclass <chemaclass@outlook.es>
2024-06-16 13:37:28 +0200

    refactor: extract exec_assert to main.sh

--- src/main.sh
+++ src/main.sh
@@ -9,3 +9,13 @@ function main::exec_tests() {
   console_results::render_result
   exit 0
 }
+
+function main::exec_assert() {
+  local assert_fn=$1
+  local args=("${@:2}") # Store all arguments starting from the second as an array
+
+  "$assert_fn" "${args[@]}"
+  if [[ "$(state::get_tests_failed)" -gt 0 ]] || [[ "$(state::get_assertions_failed)" -gt 0 ]]; then
+      exit 1
+  fi
+}
