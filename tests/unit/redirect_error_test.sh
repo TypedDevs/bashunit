37b66885cfb2806ef8526a75ff1b5d2ca11671f2
Chemaclass <chemaclass@outlook.es>
2024-06-23 00:10:39 +0200

    test: add redirect_error_without_log

--- tests/unit/redirect_error_test.sh
+++ tests/unit/redirect_error_test.sh
@@ -6,13 +6,7 @@ function tear_down() {
   rm $_ERROR_LOG
 }
 
-function test_redirect_error() {
-
-  function render_into_error_fd_and_exit() {
-    echo "$*" >&2
-    exit 1
-  }
-
+function test_redirect_error_with_log() {
   exec 2>&3 2>$_ERROR_LOG
 
   _="$(render_into_error_fd_and_exit "arg1" "arg2")"
@@ -21,4 +15,16 @@ function test_redirect_error() {
   local error_output
   error_output=$(<$_ERROR_LOG)
   assert_equals "arg1 arg2" "$error_output"
+}
+
+function test_redirect_error_without_log() {
+  exec 2>&3 2>/dev/null
+
+  _="$(render_into_error_fd_and_exit "...args")"
+  assert_general_error
+}
+
+function render_into_error_fd_and_exit() {
+  echo "$*" >&2
+  exit 1
 }
