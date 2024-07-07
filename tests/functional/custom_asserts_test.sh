bd988b6e69464f46910539f13a15f0d40e9f5c6f
Chemaclass <chemaclass@outlook.es>
2024-07-08 01:09:35 +0200

    refactor: rename test names

--- tests/functional/custom_asserts_test.sh
+++ tests/functional/custom_asserts_test.sh
@@ -5,21 +5,21 @@ function set_up() {
   source "$_ROOT_DIR/custom_asserts.sh"
 }
 
-function test_assert_foo_success() {
+function test_assert_foo_passed() {
   assert_foo "foo"
 }
 
-function test_assert_foo_failing() {
+function test_assert_foo_failed() {
   assert_equals\
     "$(console_results::print_failed_test "Assert foo" "foo" "but got" "bar")"\
     "$(assert_foo "bar")"
 }
 
-function test_assert_positive_number_success() {
+function test_assert_positive_number_passed() {
   assert_positive_number "1"
 }
 
-function test_assert_positive_number_failing() {
+function test_assert_positive_number_failed() {
   assert_equals\
     "$(console_results::print_failed_test "Assert positive number" "positive number" "got" "0")"\
     "$(assert_positive_number "0")"
