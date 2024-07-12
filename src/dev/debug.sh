5d615d8aed23f80cbae0e68734b07e25be977c21
Chemaclass <chemaclass@outlook.es>
2024-07-12 22:36:34 +0200

    chore: simplify dump function

--- src/dev/debug.sh
+++ src/dev/debug.sh
@@ -4,7 +4,6 @@
 # This is debug function; do not use in prod!
 function dump() {
   echo "[DEBUG] ${BASH_SOURCE[1]}:${BASH_LINENO[0]}:" "$@"
-  set -x # enable bash debug, to have a deeper context
 }
 
 # Dump and Die.
