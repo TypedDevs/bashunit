f2258a2fd9ba293b5fc46ebd7ef563baa4353b98
Chemaclass <chemaclass@outlook.es>
2024-07-12 17:03:23 +0200

    feat: add debug.sh with dump and dd functions

---
+++ src/local/debug.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# An alternative to echo when debugging.
+# This is debug function; do not use in prod!
+function dump() {
+  echo "[DEBUG] ${BASH_SOURCE[1]}:${BASH_LINENO[0]}:" "$@"
+  set -x
+}
+
+# Dump and Die.
+function dd() {
+  dump "$@"
+  exit 1
+}
