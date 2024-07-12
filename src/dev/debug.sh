9c1e84ecb4f64f1566f57dfd0595f9bddee55b04
Chemaclass <chemaclass@outlook.es>
2024-07-12 22:58:28 +0200

    chore: improve debug functions

--- src/dev/debug.sh
+++ src/dev/debug.sh
@@ -3,11 +3,16 @@
 # An alternative to echo when debugging.
 # This is debug function; do not use in prod!
 function dump() {
-  echo "[DEBUG] ${BASH_SOURCE[1]}:${BASH_LINENO[0]}:" "$@"
+  printf "[%s] %s: %s\n" "${_COLOR_SKIPPED}DUMP${_COLOR_DEFAULT}" \
+    "${_COLOR_PASSED}${BASH_SOURCE[1]}:${BASH_LINENO[0]}" \
+    "${_COLOR_DEFAULT}$*"
 }
 
 # Dump and Die.
 function dd() {
-  dump "$@"
-  exit 1
+  printf "[%s] %s: %s\n" "${_COLOR_FAILED}DUMP${_COLOR_DEFAULT}" \
+    "${_COLOR_PASSED}${BASH_SOURCE[1]}:${BASH_LINENO[0]}" \
+    "${_COLOR_DEFAULT}$*"
+
+  kill -9 $$
 }
