36cd586dbf98cc9c800da20e60d4998a82b6e977
Chemaclass <chemaclass@outlook.es>
2024-07-23 14:58:34 +0200

    chore: keep $ORIGINAL_TERM after using dumb for acceptance tests

--- tests/acceptance/bashunit_test.sh
+++ tests/acceptance/bashunit_test.sh
@@ -2,8 +2,16 @@
 set -euo pipefail
 
 function set_up_before_script() {
-  TERM=dumb
+  ORIGINAL_TERM=$TERM
   TEST_ENV_FILE="tests/acceptance/fixtures/.env.default"
+}
+
+function set_up() {
+  TERM=dumb
+}
+
+function tear_down() {
+  TERM=$ORIGINAL_TERM
 }
 
 function test_bashunit_should_display_version() {
