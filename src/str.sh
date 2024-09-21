cecf401256b5377f55df3253489c2639a297718b
Chemaclass <chemaclass@outlook.es>
2024-09-21 17:45:46 +0200

    fix: str::rpad()

--- src/str.sh
+++ src/str.sh
@@ -3,16 +3,8 @@
 function str::rpad() {
   local left_text="$1"
   local right_word="$2"
-  local width_padding="${3:-(($TERMINAL_WIDTH - 5))}"
-
-  if [[ -z "$left_text" ]]; then
-    local padding=$((width_padding - ${#right_word}))
-    printf "%*s" "$padding" "$right_word"
-    return
-  fi
-
-  local total_length=$(( ${#left_text} + ${#right_word} ))
-  local padding=$((width_padding - total_length))
+  local width_padding="${3:-(($TERMINAL_WIDTH))}"
+  local padding=$((width_padding - ${#left_text}))
 
   printf "%s%*s" "$left_text" "$padding" "$right_word"
 }
