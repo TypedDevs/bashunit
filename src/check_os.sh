194853fb81d6e79146cb4f1f9fe52638334587a3
Chemaclass <chemaclass@outlook.es>
2024-09-21 22:39:38 +0200

    feat: distinguish Linux - Ubuntu,Alpine,Other

--- src/check_os.sh
+++ src/check_os.sh
@@ -4,9 +4,15 @@
 _OS="Unknown"
 
 if [[ "$(uname)" == "Linux" ]]; then
-  _OS="Linux"
+    if command -v apt > /dev/null; then
+        _OS="Linux - Ubuntu"
+    elif command -v apk > /dev/null; then
+        _OS="Linux - Alpine"
+    else
+        _OS="Linux - Other"
+    fi
 elif [[ "$(uname)" == "Darwin" ]]; then
-  _OS="OSX"
-elif [[ $(uname) == *"MINGW"* ]]; then
-  _OS="Windows"
+    _OS="OSX"
+elif [[ "$(uname)" == *"MINGW"* ]]; then
+    _OS="Windows"
 fi
