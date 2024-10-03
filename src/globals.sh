3fcceaa5a65fa1ddf82ec261d36d2961062327d0
Chemaclass <chemaclass@outlook.es>
2024-10-03 18:13:55 +0200

    feat: adding more global functions

--- src/globals.sh
+++ src/globals.sh
@@ -1,10 +1,53 @@
 #!/bin/bash
 set -euo pipefail
 
+# This file provides a set of global functions to developers.
+
 function current_dir() {
   dirname "${BASH_SOURCE[1]}"
 }
 
 function current_filename() {
   basename "${BASH_SOURCE[1]}"
+}
+
+function current_timestamp() {
+  date +"%Y-%m-%d %H:%M:%S"
+}
+
+function is_command_available() {
+  command -v "$1" >/dev/null 2>&1
+}
+
+function random_str() {
+  local length=${1:-6}
+  LC_ALL=C tr -dc A-Za-z0-9 </dev/urandom | head -c "$length"
+}
+
+function temp_file() {
+  # shellcheck disable=SC2155
+  local path="/tmp/bashunit_temp.$(random_str)"
+  touch "$path"
+  echo "$path"
+}
+
+function temp_dir() {
+  # shellcheck disable=SC2155
+  local dir="/tmp/bashunit_tempdir.$(random_str 5)}"
+  mkdir -p "$dir"
+  echo "$dir"
+}
+
+function cleanup_temp_files() {
+  rm -rf /tmp/bashunit_temp*
+}
+
+function log_info() {
+  # shellcheck disable=SC2145
+  echo "$(current_timestamp) [INFO]: $@"
+}
+
+function log_error() {
+  # shellcheck disable=SC2145
+  echo "$(current_timestamp) [ERROR]: $@" >&2
 }
--- tests/unit/globals_test.sh
+++ tests/unit/globals_test.sh
@@ -8,3 +8,53 @@ function test_globals_current_dir() {
 function test_globals_current_filename() {
   assert_same "globals_test.sh" "$(current_filename)"
 }
+
+function test_globals_current_timestamp() {
+  assert_matches \
+    "^[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}$" \
+    "$(current_timestamp)"
+}
+
+function test_globals_is_command_available() {
+  assert_successful_code "$(is_command_available ls)"
+}
+
+function test_globals_is_command_not_available() {
+  assert_general_error "$(is_command_available non-existing-command)"
+}
+
+function test_globals_random_str_default() {
+  assert_matches "^[A-Za-z0-9]{6}$" "$(random_str)"
+}
+
+function test_globals_random_str_custom() {
+  assert_matches "^[A-Za-z0-9]{3}$" "$(random_str 3)"
+}
+
+function test_globals_temp_file() {
+  # shellcheck disable=SC2155
+  local temp_file=$(temp_file)
+  assert_file_exists "$temp_file"
+  cleanup_temp_files
+  assert_file_not_exists "$temp_file"
+}
+
+function test_globals_temp_dir() {
+  # shellcheck disable=SC2155
+  local temp_dir=$(temp_dir)
+  assert_directory_exists "$temp_dir"
+  cleanup_temp_files
+  assert_directory_not_exists "$temp_dir"
+}
+
+function test_globals_log_info() {
+  assert_matches \
+    "^[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2} \[INFO\]: hello, world$" \
+    "$(log_info "hello," "world")"
+}
+
+function test_globals_log_error() {
+  assert_matches \
+    "^[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2} \[ERROR\]: hello, luna$" \
+    "$(log_error "hello," "luna" 2>&1)"
+}
