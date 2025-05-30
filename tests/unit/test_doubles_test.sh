d8ced706922ab915276c4f0ff00b3495c7fa5c06
Chemaclass <chemaclass@outlook.es>
2025-05-31 01:12:08 +0200

    fix: linter

--- tests/unit/test_doubles_test.sh
+++ tests/unit/test_doubles_test.sh
@@ -148,18 +148,20 @@ function test_spy_called_with_different_arguments() {
   assert_have_been_called_with "second" ps 2
 }
 
-function test_successful_not_called() {
+function test_spy_successful_not_called() {
   spy ps
 
   assert_not_called ps
 }
 
-function test_unsuccessful_not_called() {
+function test_spy_unsuccessful_not_called() {
   spy ps
 
   ps
 
   assert_same \
-    "$(console_results::print_failed_test \"Unsuccessful not called\" \"ps\" \"to has been called\" \"0 times\" \"actual\" \"1 times\")" \
+    "$(console_results::print_failed_test "Unsuccessful not called" "ps" \
+      "to has been called" "0 times" \
+      "actual" "1 times")" \
     "$(assert_not_called ps)"
 }
