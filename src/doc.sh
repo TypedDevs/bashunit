8d32a4650aaca05dda749e14a666110e26f5830b
Chemaclass <chemaclass@outlook.es>
2025-07-20 14:05:03 +0200

    refactor: optimize doc::print_asserts

--- src/doc.sh
+++ src/doc.sh
@@ -4,29 +4,34 @@ function doc::print_asserts() {
   local filter="${1:-}"
   local doc_file="$BASHUNIT_ROOT_DIR/docs/assertions.md"
   local line
-  local print=0
   local docstring=""
+  local fn=""
+  local should_print=0
+
   while IFS='' read -r line || [[ -n "$line" ]]; do
     if [[ $line =~ ^##\ ([A-Za-z0-9_]+) ]]; then
-      local fn="${BASH_REMATCH[1]}"
+      fn="${BASH_REMATCH[1]}"
       if [[ -z "$filter" || "$fn" == *"$filter"* ]]; then
-        print=1
+        should_print=1
         echo "$line"
         docstring=""
       else
-        print=0
+        should_print=0
       fi
       continue
     fi
 
-    if [[ $print -eq 1 ]]; then
+    if (( should_print )); then
       if [[ "$line" =~ ^\`\`\` ]]; then
-        print=0
         echo "--------------"
         echo "$docstring"
+        should_print=0
         continue
       fi
+
       [[ "$line" == "::: code-group" ]] && continue
+
+      # Remove markdown link brackets and anchor tags
       line="${line//[\[\]]/}"
       line="$(sed -E 's/ *\(#[-a-z0-9]+\)//g' <<< "$line")"
       docstring+="$line"$'\n'
