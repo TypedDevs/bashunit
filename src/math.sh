1ed1ca85b59b22b0b41f3b499bd3d16b76879448
Chemaclass <chemaclass@outlook.es>
2025-07-27 04:04:46 +0200

    refactor: src/math.sh

--- src/math.sh
+++ src/math.sh
@@ -1,20 +1,25 @@
 #!/usr/bin/env bash
-# shellcheck disable=SC2155
 
 function math::calculate() {
+  local expr="$*"
+
   if dependencies::has_bc; then
-    echo "$*" | bc
-  elif [[ "$*" == *.* ]] && dependencies::has_awk; then
-    awk "BEGIN { print ($*) }"
-  elif [[ "$*" == *.* ]]; then
-    # Strip decimal parts and leading zeros
-    local expression=$(echo "$*" | sed -E 's/([0-9]+)\.[0-9]+/\1/g' | sed -E 's/\b0*([1-9][0-9]*)/\1/g')
-    local result=$(( expression ))
-    echo "$result"
-  else
-    # Strip leading zeros even for purely integer math
-    local expression=$(echo "$*" | sed -E 's/\b0*([1-9][0-9]*)/\1/g')
-    local result=$(( expression ))
-    echo "$result"
+    echo "$expr" | bc
+    return
   fi
+
+  if [[ "$expr" == *.* ]]; then
+    if dependencies::has_awk; then
+      awk "BEGIN { print ($expr) }"
+      return
+    fi
+    # Downgrade to integer math by stripping decimals
+    expr=$(echo "$expr" | sed -E 's/([0-9]+)\.[0-9]+/\1/g')
+  fi
+
+  # Remove leading zeros from integers
+  expr=$(echo "$expr" | sed -E 's/\b0*([1-9][0-9]*)/\1/g')
+
+  local result=$(( expr ))
+  echo "$result"
 }
