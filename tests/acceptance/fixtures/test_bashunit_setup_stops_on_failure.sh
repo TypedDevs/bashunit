d7659da3c48185ef2424ec01a64180e84691396b
Chemaclass <chemaclass@outlook.es>
2025-11-28 15:41:20 +0100

    fix: stop executing remaining commands in set_up/tear_down after first failure

---
+++ tests/acceptance/fixtures/test_bashunit_setup_stops_on_failure.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Test fixture for issue #517: When set_up fails, remaining commands should not execute
+
+function set_up() {
+  # This command will fail
+  false
+  # This should NOT execute - if it does, the marker file will be created
+  touch "/tmp/bashunit_setup_marker_test"
+}
+
+function test_dummy() {
+  assert_same "foo" "foo"
+}
