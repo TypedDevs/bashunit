2932b14f8021f407343b3a59d00c76dc2d067b61
Chemaclass <chemaclass@outlook.es>
2025-12-04 01:11:14 +0100

    fix: linter in custom_assertions_test.sh

--- tests/unit/custom_assertions_test.sh
+++ tests/unit/custom_assertions_test.sh
@@ -42,6 +42,7 @@ function test_custom_assertion_with_fail_shows_correct_test_name() {
   output="$(
     # Temporarily override state::print_line to capture output
     _captured_output=""
+    # shellcheck disable=SC2317,SC2329
     state::print_line() {
       _captured_output="$2"
       echo "$_captured_output"
@@ -54,8 +55,7 @@ function test_custom_assertion_with_fail_shows_correct_test_name() {
     echo "$_captured_output"
   )"
 
-  # The output should contain the test function name
-  # "Custom assertion with fail shows correct test name" (normalized from test_custom_assertion_with_fail_shows_correct_test_name)
+  # The output should contain the test function name (normalized from the test function)
   assert_contains "Custom assertion with fail shows correct test name" "$output"
   assert_not_contains "Assert valid json" "$output"
 }
@@ -66,6 +66,7 @@ function test_custom_assertion_with_bashunit_assertion_failed_shows_correct_test
   local output
   output="$(
     _captured_output=""
+    # shellcheck disable=SC2317,SC2329
     state::print_line() {
       _captured_output="$2"
       echo "$_captured_output"
@@ -77,7 +78,8 @@ function test_custom_assertion_with_bashunit_assertion_failed_shows_correct_test
     echo "$_captured_output"
   )"
 
-  assert_contains "Custom assertion with bashunit assertion failed shows correct test name" "$output"
+  assert_contains \
+    "Custom assertion with bashunit assertion failed shows correct test name" "$output"
   assert_not_contains "Assert positive number" "$output"
 }
 
@@ -87,6 +89,7 @@ function test_custom_assertion_calling_assert_same_shows_correct_test_name() {
   local output
   output="$(
     _captured_output=""
+    # shellcheck disable=SC2317,SC2329
     state::print_line() {
       _captured_output="$2"
       echo "$_captured_output"
@@ -113,35 +116,32 @@ function test_helper_find_test_function_name_finds_test() {
 
 function test_helper_find_test_function_name_from_nested_function() {
   # Test that helper::find_test_function_name works from nested functions
-  local inner_function
-  inner_function() {
+  _inner_function() {
     helper::find_test_function_name
   }
 
   local found_name
-  found_name="$(inner_function)"
+  found_name="$(_inner_function)"
 
   assert_same "test_helper_find_test_function_name_from_nested_function" "$found_name"
 }
 
 function test_helper_find_test_function_name_from_deeply_nested() {
   # Test from deeply nested functions
-  local level1 level2 level3
-
-  level3() {
+  _level3() {
     helper::find_test_function_name
   }
 
-  level2() {
-    level3
+  _level2() {
+    _level3
   }
 
-  level1() {
-    level2
+  _level1() {
+    _level2
   }
 
   local found_name
-  found_name="$(level1)"
+  found_name="$(_level1)"
 
   assert_same "test_helper_find_test_function_name_from_deeply_nested" "$found_name"
 }
