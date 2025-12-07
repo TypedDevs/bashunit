55746da27f3359f7fb3574d016ed6cede11fc827
Chemaclass <chemaclass@outlook.es>
2025-12-07 14:18:45 +0100

    refactor!: namespace standalone functions with bashunit::
    Renamed standalone functions to prevent naming collisions:
    - sgr -> bashunit::sgr
    - dump -> bashunit::dump
    - dd -> bashunit::dd

--- src/dev/debug.sh
+++ src/dev/debug.sh
@@ -2,14 +2,14 @@
 
 # An alternative to echo when debugging.
 # This is debug function; do not use in prod!
-function dump() {
+function bashunit::dump() {
   printf "[%s] %s: %s\n" "${_BASHUNIT_COLOR_SKIPPED}DUMP${_BASHUNIT_COLOR_DEFAULT}" \
     "${_BASHUNIT_COLOR_PASSED}${BASH_SOURCE[1]}:${BASH_LINENO[0]}" \
     "${_BASHUNIT_COLOR_DEFAULT}$*"
 }
 
 # Dump and Die.
-function dd() {
+function bashunit::dd() {
   printf "[%s] %s: %s\n" "${_BASHUNIT_COLOR_FAILED}DUMP${_BASHUNIT_COLOR_DEFAULT}" \
     "${_BASHUNIT_COLOR_PASSED}${BASH_SOURCE[1]}:${BASH_LINENO[0]}" \
     "${_BASHUNIT_COLOR_DEFAULT}$*"
