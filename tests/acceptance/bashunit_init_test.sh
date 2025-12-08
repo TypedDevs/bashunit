2fc3894f438d5ee4e041bbe669a06479cdd87f81
Chemaclass <chemaclass@outlook.es>
2025-12-08 12:29:37 +0100

    fix: flaky strict tests

--- tests/acceptance/bashunit_init_test.sh
+++ tests/acceptance/bashunit_init_test.sh
@@ -3,10 +3,10 @@
 
 set -euo pipefail
 
-TMP_DIR="tmp/init"
+BASHUNIT_PATH="$PWD/bashunit"
+
 function set_up() {
-  rm -rf "$TMP_DIR"
-  mkdir -p "$TMP_DIR"
+  TMP_DIR=$(mktemp -d)
 }
 
 function tear_down() {
@@ -17,7 +17,7 @@ function test_bashunit_init_creates_structure() {
   # switch into a clean temporary directory
   pushd "$TMP_DIR" >/dev/null
   # generate test scaffolding
-  ../../bashunit init > /tmp/init.log
+  "$BASHUNIT_PATH" init > /tmp/init.log
   # perform the assertions
   assert_file_exists "tests/example_test.sh"
   assert_file_exists "tests/bootstrap.sh"
@@ -27,7 +27,7 @@ function test_bashunit_init_creates_structure() {
 
 function test_bashunit_init_custom_directory() {
   pushd "$TMP_DIR" >/dev/null
-  ../../bashunit init custom > /tmp/init.log
+  "$BASHUNIT_PATH" init custom > /tmp/init.log
   assert_file_exists "custom/example_test.sh"
   assert_file_exists "custom/bootstrap.sh"
   popd >/dev/null
@@ -38,7 +38,7 @@ function test_bashunit_init_updates_env() {
 
   pushd "$TMP_DIR" >/dev/null
   echo "BASHUNIT_BOOTSTRAP=old/bootstrap.sh" > .env
-  ../../bashunit init custom > /tmp/init.log
+  "$BASHUNIT_PATH" init custom > /tmp/init.log
   assert_file_exists "custom/example_test.sh"
   assert_file_exists "custom/bootstrap.sh"
   assert_file_contains .env "#BASHUNIT_BOOTSTRAP=old/bootstrap.sh"
